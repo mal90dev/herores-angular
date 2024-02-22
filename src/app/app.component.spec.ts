66ab62b26b2a4d0a3f2bc4da07dbfe105414000f
Mario Aguilar <mal90developer@gmail.com>
2024-02-22 09:35:18 +0100

    init migration

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,32 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { SpinnerComponent } from './shared/components/spinner/spinner.component';
-
 describe('AppComponent', () => {
+
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-        HeaderComponent,
-        SpinnerComponent
-      ]
+      declarations: [AppComponent],
     }).compileComponents();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
-  it('should be created correctly', () => {
+  it('should render component', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('should contain a router-outlet', () => {
-    const routerOutlet = fixture.nativeElement.querySelector('router-outlet');
-    expect(routerOutlet).toBeTruthy();
   });
 
 });
